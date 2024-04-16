9b3a95a645c560e5d98fb3503adb6f8f4844eea5
Rohanalidevops <167162988+Rohanalidevops@users.noreply.github.com>
2024-04-16 11:38:43 +0530

    Add files via upload

---
+++ video delete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd Videos/;ls
+# Execute the find and remove command
+if find /home/mpokket/Videos/ -type f -name '*.mkv' ! -newermt "$(date -d 'today 00:00' +%Y-%m-%d\ %H:%M)" -exec rm {} +; then echo "Command executed successfully."
+else echo "Error executing the command."
+fi
+
+
+
+
+
+Run as crontab
+------------------
+00 18 * * *  /script/Delete.sh;./Delete.sh
