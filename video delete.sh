ded3314b5027076007015532efed82abffb6420f
Rohanalidevopos <rohanaws2022@gmail.com>
2024-06-10 16:01:19 +0530

    Date 10-05-2024

--- video delete.sh
+++ video delete.sh
@@ -15,3 +15,20 @@ Run as crontab
 
 
 find /home/mpokket/Videos/ -type f -name '*.mkv' ! -newermt "$(date -d 'yesterday 00:00' +%Y-%m-%d\ %H:%M)" -exec rm {} +
+
+
+
+#!/bin/bash
+
+# Navigate to the Videos directory
+cd /home/mpokket/Videos/ || { echo "Failed to navigate to /home/mpokket/Videos/"; exit 1; }
+
+# List the files in the Videos directory
+ls
+
+# Execute the find and remove command
+if find /home/mpokket/Videos/ -type f -name '*.mkv' ! -newermt "$(date -d '3 days ago 00:00' +%Y-%m-%d\ %H:%M)" -exec rm {} +; then
+    echo "Command executed successfully."
+else
+    echo "Error executing the command."
+fi
